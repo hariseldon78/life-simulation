75db6280c4f5d595b6419d9fc381ef80de8e319b
Roberto Previdi <hariseldon78@gmail.com>
2019-05-11 14:04:16 +0200

    WIP grid interactions, seems buggy

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -2,11 +2,26 @@
 	<div>
 		<div class="canvas" ref="canvas"></div>
 		<button @click="addInsect">Insect</button>
-        <p>Food:
-            <button @click="foodTimer-=20;resetFoodGenerator()">+</button>
-            <button @click="foodTimer+=20;resetFoodGenerator()">-</button>
-            {{foodTimer}}
-        </p>
+		<p>
+			Food:
+			<button
+				@click="
+					foodTimer -= 20;
+					resetFoodGenerator();
+				"
+			>
+				+
+			</button>
+			<button
+				@click="
+					foodTimer += 20;
+					resetFoodGenerator();
+				"
+			>
+				-
+			</button>
+			{{ foodTimer }}
+		</p>
 		<div>
 			<span v-for="(insect, index) in insects" :key="index">
 				insect health: {{ Math.floor(insect.health) }} -
@@ -22,6 +37,7 @@ import { SquareInsect } from '@/models/SquareInsect';
 import { Food } from '@/models/Food';
 import { SceneObject } from '@/models/SceneObject';
 import * as _ from 'lodash';
+import { gridInteractions } from '@/models/grid';
 
 function rollup(v: number, max: number): number {
 	while (v > max) {
@@ -33,17 +49,6 @@ function rollup(v: number, max: number): number {
 	return v;
 }
 
-function overlap(a: Two.BoundingClientRect, b: Two.BoundingClientRect): boolean {
-	function between(a: number, x: number, b: number): boolean {
-		const min = Math.min(a, b);
-		const max = Math.max(a, b);
-		return min <= x && x <= max;
-	}
-	const xOverlap = between(b.left, a.left, b.right) || between(b.left, a.right, b.right);
-	const yOverlap = between(b.top, a.top, b.bottom) || between(b.top, a.bottom, b.bottom);
-	return xOverlap && yOverlap;
-}
-
 @Component
 export default class Scene extends Vue {
 	objects: SceneObject[] = [];
@@ -51,10 +56,10 @@ export default class Scene extends Vue {
 		fullscreen: false,
 		autostart: true,
 	});
-	foodTimer=500;
+	foodTimer = 500;
 
-	children: SceneObject[]=[];
-	foodGenerator: number=0;
+	children: SceneObject[] = [];
+	foodGenerator: number = 0;
 	mounted() {
 		let canvas = this.$refs.canvas as HTMLElement;
 		this.two.appendTo(canvas);
@@ -76,11 +81,11 @@ export default class Scene extends Vue {
 			this.children.forEach(c => this.objects.push(c));
 			this.children = [];
 		});
-		this.resetFoodGenerator()
+		this.resetFoodGenerator();
 	}
-    resetFoodGenerator(){
+	resetFoodGenerator() {
 		clearInterval(this.foodGenerator);
-		this.foodGenerator=setInterval(() => this.addFood(), this.foodTimer);
+		this.foodGenerator = setInterval(() => this.addFood(), this.foodTimer);
 	}
 	addInsect() {
 		this.objects.push(
@@ -104,23 +109,13 @@ export default class Scene extends Vue {
 		);
 	}
 	computeCollisions() {
-		const part=_.partition(this.objects,obj=>obj.health>0);
+		const part = _.partition(this.objects, obj => obj.health > 0);
 		this.objects = part[0];
-		part[1].forEach(obj=>this.two.remove(obj));
+		part[1].forEach(obj => this.two.remove(obj));
 
-		const boxes = this.objects.map(o => o.boundingBox());
-		for (let i = 0; i < boxes.length; i++) {
-			if (!this.objects[i].movable) continue;
-			const boxa = boxes[i];
-			for (let j = 0; j < boxes.length; j++) {
-				const boxb = boxes[j];
-				if (boxa === boxb) continue;
-				if (overlap(boxa, boxb)) {
-					this.objects[i].collision(this.objects[j]);
-					this.objects[j].collision(this.objects[i]);
-				}
-			}
-		}
+		gridInteractions(this.two, this.objects).forEach(int =>
+			this.objects[int.a].collision(this.objects[int.b])
+		);
 	}
 }
 </script>
--- src/models/grid.ts
+++ src/models/grid.ts
@@ -1,29 +1,63 @@
-import {SceneObject} from '@/models/SceneObject';
+import { SceneObject } from '@/models/SceneObject';
 import Two from 'two.js';
-
-function center(b:Two.BoundingClientRect){
-	return {x:b.left+b.width/2,y:b.top+b.height/2}
+import * as _ from 'lodash';
+function center(b: Two.BoundingClientRect) {
+	return { x: b.left + b.width / 2, y: b.top + b.height / 2 };
+}
+export function overlap(a: Two.BoundingClientRect, b: Two.BoundingClientRect): boolean {
+	function between(a: number, x: number, b: number): boolean {
+		const min = Math.min(a, b);
+		const max = Math.max(a, b);
+		return min <= x && x <= max;
+	}
+	const xOverlap = between(b.left, a.left, b.right) || between(b.left, a.right, b.right);
+	const yOverlap = between(b.top, a.top, b.bottom) || between(b.top, a.bottom, b.bottom);
+	return xOverlap && yOverlap;
 }
 
-export class Grid{
-	cells:number[][][]=[];
-	constructor(two:Two,objects:SceneObject[]){
-		for(let x=0;x<Math.ceil(two.width/150);x++) {
-			this.cells.push([]);
-			for (let y=0;y<Math.ceil(two.height/150);y++) {
-				this.cells[x].push([]);
+export interface Interaction {
+	a: number;
+	b: number;
+}
+export function gridInteractions(two: Two, objects: SceneObject[]):Interaction[] {
+	const cells: number[][][] = [];
+	let interactions: Interaction[] = [];
+	for (let x = 0; x < Math.ceil(two.width / 150); x++) {
+		cells.push([]);
+		for (let y = 0; y < Math.ceil(two.height / 150); y++) {
+			cells[x].push([]);
+		}
+	}
+	const boxes = objects.map(o => o.boundingBox());
+
+	boxes.forEach((b, i) => {
+		const c = center(b);
+		const row = Math.floor(c.y / 150);
+		const col = Math.floor(c.x / 150);
+		cells[col][row].push(i);
+	});
+
+	for (let x = 1; x < cells.length; x++) {
+		for (let y = 1; y < cells[0].length; y++) {
+			const group = [
+				...cells[x - 1][y - 1],
+				...cells[x - 1][y],
+				...cells[x][y - 1],
+				...cells[x][y],
+			];
+			for (let i = 0; i < group.length; i++) {
+				if (!objects[group[i]].movable) continue;
+				const boxa = boxes[group[i]];
+				for (let j = 0; j < group.length; j++) {
+					const boxb = boxes[group[j]];
+					if (group[i] === group[j]) continue;
+					if (overlap(boxa, boxb)) {
+						interactions.push({a: group[i], b: group[j]});
+					}
+				}
 			}
 		}
-		const boxes = objects.map(o => o.boundingBox());
-
-		boxes.forEach((b,i)=>{
-			const c=center(b);
-			const row=Math.floor(c.y/150);
-			const col=Math.floor(c.x/150);
-			this.cells[col][row].push(i);
-		})
-
-
 	}
 
+	return _.uniq(interactions);
 }
